b51dfd5f5c3e64f0c3305b83d87bf539b3766a6b
eommx22 <eo.22.mmx@gmail.com>
2023-01-26 10:16:12 +0100

    adding API1

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.index, name='index')
+    #path('', views.index, name='index')
+    path('items/', views.MenuItemsView.as_view()),
+    path('items/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, DestroyAPIView
+from .models import Menu
+from .serializers import MenuItemSerializer
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+# def index(request):
+#     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset= Menu.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+    #lookup_fields = ['title']
